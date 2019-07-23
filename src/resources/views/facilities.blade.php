0327e617c3ccad5e63b9074fc7d68769d4d4c3ac
suomi0627 <suomi0627@gmail.com>
2019-07-23 10:29:09 +0900

    コードをフォーマットした

--- src/resources/views/facilities.blade.php
+++ src/resources/views/facilities.blade.php
@@ -2,74 +2,75 @@
 
 @section('content')
 
-    <!-- Bootstrapの定形コード… -->
+<!-- Bootstrapの定形コード… -->
+
+<div class="panel-body">
+    <!-- バリデーションエラーの表示 -->
+    @include('common.errors')
+
+    <!-- 新施設フォーム -->
+    <form action="/facilities" method="POST" class="form-horizontal">
+        {{ csrf_field() }}
+
+        <!-- 施設名 -->
+        <div class="form-group">
+            <label for="facilities" class="col-sm-3 control-label">施設名</label>
+
+            <div class="col-sm-6">
+                <input type="text" name="name" id="facilities-name" class="form-control">
+            </div>
+        </div>
+
+        <!-- 施設追加ボタン -->
+        <div class="form-group">
+            <div class="col-sm-offset-3 col-sm-6">
+                <button type="submit" class="btn btn-default">
+                    <i class="fa fa-plus"></i> 施設追加
+                </button>
+            </div>
+        </div>
+    </form>
+</div>
+
+<!-- 現在の施設 -->
+@if (count($facilities) > 0)
+<div class="panel panel-default">
+    <div class="panel-heading">
+        施設名
+    </div>
 
     <div class="panel-body">
-        <!-- バリデーションエラーの表示 -->
-        @include('common.errors')
+        <table class="table table-striped facilities-table">
 
-        <!-- 新施設フォーム -->
-        <form action="/facilities" method="POST" class="form-horizontal">
-            {{ csrf_field() }}
+            <!-- テーブルヘッダー -->
+            <thead>
+                <th>施設一覧</th>
+                <th>&nbsp;</th>
+            </thead>
 
-            <!-- 施設名 -->
-            <div class="form-group">
-                <label for="facilities" class="col-sm-3 control-label">施設名</label>
+            <!-- テーブルボディー -->
+            <tbody>
+                @foreach ($facilities as $facilities)
+                <tr>
+                    <!-- 施設名 -->
+                    <td class="table-text">
+                        <div>{{ $facilities->name }}</div>
+                    </td>
 
-                <div class="col-sm-6">
-                    <input type="text" name="name" id="facilities-name" class="form-control">
-                </div>
-            </div>
+                    <td>
+                        <!-- TODO: 削除ボタン -->
+                        <form action="/facilities/{{ $facilities->id }}" method="POST">
+                            {{ csrf_field() }}
+                            {{ method_field('DELETE') }}
 
-            <!-- 施設追加ボタン -->
-            <div class="form-group">
-                <div class="col-sm-offset-3 col-sm-6">
-                    <button type="submit" class="btn btn-default">
-                        <i class="fa fa-plus"></i> 施設追加
-                    </button>
-                </div>
-            </div>
-        </form>
+                            <button>×</button>
+                        </form>
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
     </div>
-        <!-- 現在の施設 -->
-	    @if (count($facilities) > 0)
-	    <div class="panel panel-default">
-	        <div class="panel-heading">
-	            施設名
-	        </div>
-
-	        <div class="panel-body">
-	            <table class="table table-striped facilities-table">
-
-	                <!-- テーブルヘッダー -->
-	                <thead>
-	                    <th>施設一覧</th>
-	                    <th>&nbsp;</th>
-	                </thead>
-
-	                <!-- テーブルボディー -->
-	                <tbody>
-	                    @foreach ($facilities as $facilities)
-	                        <tr>
-	                            <!-- 施設名 -->
-	                            <td class="table-text">
-	                                <div>{{ $facilities->name }}</div>
-	                            </td>
-
-	                            <td>
-                                    <!-- TODO: 削除ボタン -->
-<form action="/facilities/{{ $facilities->id }}" method="POST">
-    {{ csrf_field() }}
-    {{ method_field('DELETE') }}
-
-    <button>×</button>
-  </form>
-	                            </td>
-	                        </tr>
-	                    @endforeach
-	                </tbody>
-	            </table>
-	        </div>
-	    </div>
-	@endif
+</div>
+@endif
 @endsection
