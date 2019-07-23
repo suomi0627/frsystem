f574c348be0c8398e043c897629e02d304f8d2e6
suomi0627 <suomi0627@gmail.com>
2019-07-23 12:19:18 +0900

    ナビバーを横幅いっぱいに設定した

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -1,30 +1,35 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-            <meta charaset="utf-8">
-	        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-	        <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-        <title>Laravel Quickstart - Basic</title>
+<head>
+    <meta charaset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css">
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700">
+    <title>Laravel Quickstart - Basic</title>
 
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-    </head>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700">
 
-    </head>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+</head>
 
-    <body>
-        <div class="container">
+</head>
 
-{{-- お試し --}}
-<h1 class="text-center">施設管理システム</H1>
-{{-- お試し --}}
-        </div>
+<body>
 
-        @yield('content')
 
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-    </body>
+    <nav class="navbar navbar-default" style="background-color:#fff">
+        <!-- ナビバーの内容 -->
+        <a class="navbar-brand" href="#">
+            FRsystem
+            <i class="fa fa-pencil"></i></h1>
+        </a>
+    </nav>
+
+    @yield('content')
+
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+</body>
+
 </html>
