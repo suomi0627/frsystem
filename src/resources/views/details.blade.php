9bfb21bc9f6c99bac22c1ebb4ad2aa12d684edc2
suomi0627 <suomi0627@gmail.com>
2019-07-24 15:16:15 +0900

    とりあえず動く予約詳細のviewを作った

--- src/resources/views/details.blade.php
+++ src/resources/views/details.blade.php
@@ -1,20 +1,27 @@
-<html>
-    <head>
-        <title>アプリ名 - @yield('title')</title>
-    </head>
-    <body>
-        <header>
-            ヘッダー
-        </header>
+@extends('layouts.app')
 
-        <main>
-<input type="date">
+@section('content')
 
-            @yield('content')
-        </main>
+<!-- Bootstrapの定形コード… -->
 
-        <footer>
-            フッター
-        </footer>
-    </body>
-</html>
+<div class="panel-body">
+    <!-- バリデーションエラーの表示 -->
+    @include('common.errors')
+</div>
+
+<!-- 現在の施設 -->
+<div class="container">
+    <div class="panel panel-default">
+        <div class="panel-heading">
+            施設名
+        </div>
+
+        <div>
+            <div class="panel-body">
+                <table class="table table-striped facilities-table">
+
+                    <!-- テーブルヘッダー -->
+                    <thead>
+                        <th>予約詳細</th>
+                        <th>&nbsp;</th>
+                    </thead>
