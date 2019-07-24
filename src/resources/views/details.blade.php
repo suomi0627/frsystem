bb0a8100ce796b53a61eac42e032b1645dfbfcf6
suomi0627 <suomi0627@gmail.com>
2019-07-24 13:01:54 +0900

    details.bladeの動きを確認した

--- src/resources/views/details.blade.php
+++ src/resources/views/details.blade.php
@@ -1,54 +1,20 @@
-@extends('layouts.app')
+<html>
+    <head>
+        <title>アプリ名 - @yield('title')</title>
+    </head>
+    <body>
+        <header>
+            ヘッダー
+        </header>
 
-@section('content')
+        <main>
+<input type="date">
 
-<!-- Bootstrapの定形コード… -->
+            @yield('content')
+        </main>
 
-<div class="panel-body">
-    <!-- バリデーションエラーの表示 -->
-    @include('common.errors')
-
-</div>
-
-<!-- 現在の施設 -->
-@if (count($details) > 0)
-<div class="panel panel-default">
-    <div class="panel-heading">
-        施設名
-    </div>
-
-    <div class="panel-body">
-        <table class="table table-striped facilities-table">
-
-            <!-- テーブルヘッダー -->
-            <thead>
-                <th>施設一覧</th>
-                <th>&nbsp;</th>
-            </thead>
-
-            <!-- テーブルボディー -->
-            <tbody>
-                @foreach ($facilities as $facilities)
-                <tr>
-                    <!-- 施設名 -->
-                    <td class="table-text">
-                        <div>{{ $facilities->name }}</div>
-                    </td>
-
-                    <td>
-                        <!-- TODO: 削除ボタン -->
-                        <form action="/facilities/{{ $facilities->id }}" method="POST">
-                            {{ csrf_field() }}
-                            {{ method_field('DELETE') }}
-
-                            <button>×</button>
-                        </form>
-                    </td>
-                </tr>
-                @endforeach
-            </tbody>
-        </table>
-    </div>
-</div>
-@endif
-@endsection
+        <footer>
+            フッター
+        </footer>
+    </body>
+</html>
