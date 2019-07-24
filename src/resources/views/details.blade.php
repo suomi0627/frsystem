865c0f8cbd12110363c8a7a1c6dd5d3e08d434a3
suomi0627 <suomi0627@gmail.com>
2019-07-24 17:28:59 +0900

    datetimepickerを選んでみた

--- src/resources/views/details.blade.php
+++ src/resources/views/details.blade.php
@@ -7,7 +7,25 @@
 <div class="panel-body">
     <!-- バリデーションエラーの表示 -->
     @include('common.errors')
-
 </div>
 
+<div class="container">
+
+    <h4>施設XXX</h4>
+
+    <form action="xxx.php" method="post">
+        <div>日付:</div><input type="date" name="date"></label>
+        <input type="submit" value="送信">
+    </form>
+
+    <form>
+        <h5 id="target_title">Fromより過去日付は、Toで指定出来ません。</h5>
+        <label>From:</label>
+        <input type="time" id="timepickerFrom" placeholder="クリックして下さい" />
+
+        <label>To:</label>
+        <input type="time" id="timepickerTo" placeholder="クリックして下さい" />
+    </form>
+
+</div>
 @endsection
