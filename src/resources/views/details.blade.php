7ed7fa9579be67f6b9cd87888765a733d6d6007c
suomi0627 <suomi0627@gmail.com>
2019-07-24 16:28:20 +0900

    ナビバー上部のマージンを消した

--- src/resources/views/details.blade.php
+++ src/resources/views/details.blade.php
@@ -7,21 +7,7 @@
 <div class="panel-body">
     <!-- バリデーションエラーの表示 -->
     @include('common.errors')
+
 </div>
 
-<!-- 現在の施設 -->
-<div class="container">
-    <div class="panel panel-default">
-        <div class="panel-heading">
-            施設名
-        </div>
-
-        <div>
-            <div class="panel-body">
-                <table class="table table-striped facilities-table">
-
-                    <!-- テーブルヘッダー -->
-                    <thead>
-                        <th>予約詳細</th>
-                        <th>&nbsp;</th>
-                    </thead>
+@endsection
