dd8873ed5b4f68f0059c663c47ea7d6453421895
suomi0627 <suomi0627@gmail.com>
2019-07-26 11:27:36 +0900

    パネルの色変更と予約完了フォームを作った

--- src/resources/views/details.blade.php
+++ src/resources/views/details.blade.php
@@ -10,8 +10,8 @@
 </div>
 
 <div class="container">
-    <div class="panel panel-default">
-        <div class="panel-heading">予約詳細</div>
+    <div class="panel panel-info">
+        <div class="panel-heading">予約申込</div>
         <div class="panel-body">
             <form>
                 <div class="form-group">
@@ -27,6 +27,28 @@
                     <input class="form-control" type="text">
                 </div>
                 <button class="btn btn-default">予約</button>
+            </form>
+        </div>
+    </div>
+</div>
+
+<div class="container">
+    <div class="panel panel-success">
+        <div class="panel-heading">予約が完了しました</div>
+        <div class="panel-body">
+            <form>
+                <div class="form-group">
+                    <label class="control-label">日付</label>
+                    <input class="form-control" type="text">
+                </div>
+                <div class="form-group">
+                    <label class="control-label">開始時間</label>
+                    <input class="form-control" type="text">
+                </div>
+                <div class="form-group">
+                    <label class="control-label">終了時間</label>
+                    <input class="form-control" type="text">
+                </div>
             </form>
         </div>
     </div>
