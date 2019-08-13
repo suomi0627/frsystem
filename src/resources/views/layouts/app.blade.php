58a2a75d75c2b4177ed216a5485bd23e5e1f6fa8
suomi0627 <suomi0627@gmail.com>
2019-08-14 03:44:58 +0900

    cssをpublicから読み込むためのパスを追記した

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -12,6 +12,7 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700">
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 </head>
 
 </head>
