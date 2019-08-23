dbd8a65d155429f5ded4d5959a66e66a7f50e023
suomi0627 <suomi0627@gmail.com>
2019-08-23 14:39:59 +0900

    テーブルデータを表示できるようにした

--- src/resources/views/details.blade.php
+++ src/resources/views/details.blade.php
@@ -15,53 +15,86 @@
         <div class="panel-heading">予約申込</div>
         <div class="panel-body">
 
-
-        <form method="POST" action="/facilities/{{ $id }}/details">
+            <form method="POST" action="/facilities/{{ $id }}/details">
                 {{ csrf_field() }}
+
                 <div class="form-group">
-                    <div>施設名</div>
                     <label class="control-label">日付</label>
-                    <input name="apply_date" class="form-control" type="text">
+                    <input name="apply_date" class="form-control" type="date">
                 </div>
+
                 <div class="form-group">
                     <label class="control-label">開始時間</label>
-                    <input name="start_at" class="form-control" type="text">
+                    <input name="start_at" class="form-control" type="time" value="10:00" step="900" min="08:00"
+                        max="20:45">
                 </div>
+
                 <div class="form-group">
                     <label class="control-label">終了時間</label>
-                    <input name="end_at" class="form-control" type="text">
+                    <input name="end_at" class="form-control" type="time" value="11:00" step="900" min="08:15"
+                        max="21:00">
                 </div>
-                <button type="submit" class="btn btn-default">予約</button>
+
+                <div>
+                    <button type="submit" class="btn btn-default">予約</button>
+                </div>
             </form>
         </div>
     </div>
 </div>
 
+
+
+
 <div class="container">
     <div class="panel panel-success">
-        <div class="panel-heading">予約が完了しました</div>
+        <div class="panel-heading">予約一覧</div>
         <div class="panel-body">
             {{-- 以下、テーブル --}}
-            {{-- 以上、テーブルデータ--}}
-            <form>
-                <div>施設名</div>
+            <!-- 予約一覧 -->
+            @if (count($details) > 0)
+            <div>
+                <table class="table facility-table">
 
-                <form>
-                    <div class="form-group">
-                        <label class="control-label">日付</label>
-                        <input class="form-control" type="text">
-                    </div>
-                    <div class="form-group">
-                        <label class="control-label">開始時間</label>
-                        <input class="form-control" type="text">
-                    </div>
-                    <div class="form-group">
-                        <label class="control-label">終了時間</label>
-                        <input class="form-control" type="text">
-                    </div>
-                </form>
-            </form>
+                    <!-- テーブルヘッダー -->
+                    <thead>
+                        <th>日付</th>
+                        <th>開始</th>
+                        <th>終了</th>
+                        <th>施設id</th>
+                        <th></th>
+                    </thead>
+
+                    <!-- テーブルボディー -->
+                    <tbody>
+                        @foreach ($details as $detail)
+                        <tr>
+                            {{-- 日にち --}}
+                            <td class="table-text text-center">
+                                <div>{{ $detail->apply_date }}</div>
+                            </td>
+
+                            {{-- 開始時間 --}}
+                            <td class="table-text text-center">
+                                <div>{{ $detail->start_at }}</div>
+                            </td>
+
+                            {{-- 終了時間 --}}
+                            <td class="table-text text-center">
+                                <div>{{ $detail->end_at }}</div>
+                            </td>
+
+                            {{-- 名前 --}}
+                            <td class="table-text text-center">
+                                <div>{{ $detail->facility_id }}</div>
+                            </td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
         </div>
+        @endif
     </div>
 </div>
 
